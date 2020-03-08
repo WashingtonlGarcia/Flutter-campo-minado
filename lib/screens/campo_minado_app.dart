c290e398537f8c7a95fa066e17245200b4cad275
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-03-08 16:41:03 -0300

    Componente Stateful

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -4,27 +4,37 @@ import 'package:flutter/material.dart';
 import '../components/resultado_widget.dart';
 import '../models/campo.dart';
 
-class CampoMinado extends StatelessWidget {
-  final Campo campo = Campo(linha: 0, coluna: 0);
+class CampoMinado extends StatefulWidget {
+  @override
+  _CampoMinadoState createState() => _CampoMinadoState();
+}
+
+class _CampoMinadoState extends State<CampoMinado> {
+  bool _venceu;
+  Tabuleiro _tabuleiro = Tabuleiro(colunas: 15, linhas: 15, qtdeBombas: 10);
   _reniciar() {
     print("reniciar");
   }
 
   _abrir(Campo c) {
-    print("abrir");
+    setState(() {
+      c.abrir();
+    });
   }
 
   _alternarMarcacao(Campo c) {
-    print("alternar");
+    setState(() {
+      c.alternarMarcacao();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ResultadoWidget(venceu: true, onReniciar: _reniciar),
+      appBar: ResultadoWidget(venceu: _venceu, onReniciar: _reniciar),
       body: Container(
           child: TabuleiroWidget(
-              tabuleiro: Tabuleiro(colunas: 15, linhas: 15, qtdeBombas: 10),
+              tabuleiro: _tabuleiro,
               onAbrir: _abrir,
               onAlternarMarcacao: _alternarMarcacao)),
     );
